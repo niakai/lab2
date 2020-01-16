6cb67e384e40961694e49c19b4b41519d79e45c3
Nia Page <NiaPage@Nias-MacBook-Air.local>
2020-01-16 12:03:33 -0800

    Replaced default info in web page

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -6,4 +6,12 @@ function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+    $("a.thumbnail").click(projecctClick);
+}
+
+function projectClick(e) {
+	//prevent the page from reloading
+	e.preventDefault();
+	//In a.n event handler, $(this) refers to the object that triggered the event
+	$(this).css("background-color" , "#7fff00");
 }
